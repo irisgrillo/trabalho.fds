fc9e00184c59226de9462b91c6bf52f542dc7eed
Íris Grillo <83309114+irisgrillo@users.noreply.github.com>
2025-05-19 16:25:53 -0300

    Add files via upload

---
+++ document.getElementById('message-btn').js
@@ -0,0 +1,7 @@
+document.getElementById('message-btn').addEventListener('click', function() {
+    const message = document.getElementById('message');
+    message.textContent = "Nós somos o que nossa mente acredita";
+    message.style.display = "block";
+    message.style.color = "#2c3e50";
+    message.style.fontWeight = "bold";
+});
